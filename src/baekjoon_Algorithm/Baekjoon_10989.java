02ea6458f442142148452b7f9a36877347db8482
KimWonJun <52263410+KimWonJun@users.noreply.github.com>
2019-07-10 22:55:30 +0900

    Update Baekjoon_10989.java
    백준 10989번문제 계수정렬 이용 시간복잡도 O(NlogN)으로는 풀리지 않는 문제라 시간복잡도가 O(N)인 계수정렬을 이용함

--- src/baekjoon_Algorithm/Baekjoon_10989.java
+++ src/baekjoon_Algorithm/Baekjoon_10989.java
@@ -8,62 +8,103 @@ import java.io.OutputStreamWriter;
 
 public class Baekjoon_10989 {
 
+//	public static void main(String[] args) throws NumberFormatException, IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//		int count = Integer.parseInt(br.readLine());
+//		int[] arr = new int[count];
+//		for(int i =0; i < arr.length;i++)
+//		{
+//			arr[i] = Integer.parseInt(br.readLine());
+//		}
+//		br.close();
+//		
+//		// 힙 구성
+//		for(int i = 1; i< count; i++)
+//		{
+//			int c = i;
+//			do {
+//				int root = (c - 1)/2;
+//				if(arr[root] < arr[c])
+//				{
+//					int temp = arr[root];
+//					arr[root] = arr[c];
+//					arr[c] = temp;
+//				}
+//				c = root;
+//			} while(c != 0);
+//		}
+//		// 정렬
+//		for(int i = count-1; i>= 0; i--)
+//		{
+//			int temp = arr[0];
+//			arr[0] = arr[i];
+//			arr[i] = temp;
+//			int root = 0;
+//			int c = 1;
+//			do {
+//				c = 2*root + 1;
+//				// 자식 중 큰 값 찾기
+//				if(c < i - 1 && arr[c] < arr[c+1]) {
+//					c++;
+//				}
+//				if(c < i && arr[root] < arr[c]) {
+//					temp = arr[root];
+//					arr[root] = arr[c];
+//					arr[c] = temp;
+//				}
+//				root = c;
+//			}while(c < i);
+//		}
+//			
+//		for(int i = 0; i < count; i++)
+//		{
+//			bw.write(arr[i] + "\n");
+//		}
+//		
+//		bw.flush();
+//		bw.close();
+//	}
+	
+	// 계수 정렬
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int count = Integer.parseInt(br.readLine());
-		int[] arr = new int[count];
-		for(int i =0; i < arr.length;i++)
-		{
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		br.close();
 		
-		// 힙 구성
-		for(int i = 1; i< count; i++)
-		{
-			int c = i;
-			do {
-				int root = (c - 1)/2;
-				if(arr[root] < arr[c])
-				{
-					int temp = arr[root];
-					arr[root] = arr[c];
-					arr[c] = temp;
-				}
-				c = root;
-			} while(c != 0);
-		}
-		// 정렬
-		for(int i = count-1; i>= 0; i--)
-		{
-			int temp = arr[0];
-			arr[0] = arr[i];
-			arr[i] = temp;
-			int root = 0;
-			int c = 1;
-			do {
-				c = 2*root + 1;
-				// 자식 중 큰 값 찾기
-				if(c < i - 1 && arr[c] < arr[c+1]) {
-					c++;
-				}
-				if(c < i && arr[root] < arr[c]) {
-					temp = arr[root];
-					arr[root] = arr[c];
-					arr[c] = temp;
-				}
-				root = c;
-			}while(c < i);
-		}
-			
+		int count = Integer.parseInt(br.readLine());
+		int[] arr = new int[10000];
+		
 		for(int i = 0; i < count; i++)
 		{
-			bw.write(arr[i] + "\n");
+			arr[Integer.parseInt(br.readLine())-1] += 1;
 		}
 		
-		bw.flush();
+		for(int i = 0; i < 10000; i++)
+		{
+			if(arr[i] != 0) {
+				for(int j = 0; j < arr[i]; j++)
+				{
+					bw.write(i+1 +"\n");
+				}
+			}
+		}
+		br.close();
 		bw.close();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
