4f2bd70d3c2c2057bebcdccc91ad176b3c05a8a7
KimWonJun <52263410+KimWonJun@users.noreply.github.com>
2019-07-10 21:59:19 +0900

    Create Baekjoon_10989.java
    백준 10989번 문제, Scanner보다 빠른 입출력방식 이용, 힙정렬 풀이

---
+++ src/baekjoon_Algorithm/Baekjoon_10989.java
@@ -0,0 +1,69 @@
+package baekjoon_Algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon_10989 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int count = Integer.parseInt(br.readLine());
+		int[] arr = new int[count];
+		for(int i =0; i < arr.length;i++)
+		{
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		br.close();
+		
+		// 힙 구성
+		for(int i = 1; i< count; i++)
+		{
+			int c = i;
+			do {
+				int root = (c - 1)/2;
+				if(arr[root] < arr[c])
+				{
+					int temp = arr[root];
+					arr[root] = arr[c];
+					arr[c] = temp;
+				}
+				c = root;
+			} while(c != 0);
+		}
+		// 정렬
+		for(int i = count-1; i>= 0; i--)
+		{
+			int temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
+			int root = 0;
+			int c = 1;
+			do {
+				c = 2*root + 1;
+				// 자식 중 큰 값 찾기
+				if(c < i - 1 && arr[c] < arr[c+1]) {
+					c++;
+				}
+				if(c < i && arr[root] < arr[c]) {
+					temp = arr[root];
+					arr[root] = arr[c];
+					arr[c] = temp;
+				}
+				root = c;
+			}while(c < i);
+		}
+			
+		for(int i = 0; i < count; i++)
+		{
+			bw.write(arr[i] + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
